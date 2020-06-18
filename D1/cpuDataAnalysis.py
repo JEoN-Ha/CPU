e8de8241f2f2713e88ad8035edbeebee13b50b8f
팽대원 <fora22@kyonggi.ac.kr>
2020-06-18 17:23:05 +0900

    기능 : 토큰화 하기 전 까지 완성

---
+++ D1/cpuDataAnalysis.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+#%matplotlib inline
+
+cpuData_csv = pd.read_csv('./cpuDataSet.csv',  encoding = 'CP949')
+#cpuData_excel = pd.read_excel('./cpuData.xlsx', encoding = 'CP949')
+
+
+cpuData_csv['명사'] = cpuData_csv['발명의 명칭'] + ' ' + cpuData_csv['요약'] + ' ' + cpuData_csv['대표청구항']
+
+
