8e01a06ebfef24b35fd9e59ecce5ca2bfd5b2d2b
팽대원 <fora22@kyonggi.ac.kr>
2020-06-18 17:28:09 +0900

    형식 : tdqm 전까지 완료

--- D1/cpuDataAnalysis.py
+++ D1/cpuDataAnalysis.py
@@ -2,11 +2,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 #%matplotlib inline
+from konlpy import Okt
 
 cpuData_csv = pd.read_csv('./cpuDataSet.csv',  encoding = 'CP949')
 #cpuData_excel = pd.read_excel('./cpuData.xlsx', encoding = 'CP949')
 
 
 cpuData_csv['명사'] = cpuData_csv['발명의 명칭'] + ' ' + cpuData_csv['요약'] + ' ' + cpuData_csv['대표청구항']
+
+cpuDict = Okt()
+
 
 
